4e99fd6e9f70e8fca88163b63f4a7092481710ca
JeremyCCox <93951833+JeremyCCox@users.noreply.github.com>
2024-01-08 21:10:02 -0700

    View & Alter Metadata for SOME tracks

--- src/Components/MusicDisplay.js
+++ src/Components/MusicDisplay.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {useState} from "react";
+import {useReducer, useState} from "react";
 import MP3Tag from "mp3tag.js"
 
 const DisplayBody = styled.div`
@@ -19,14 +19,70 @@ const MusicBox = styled.div`
   background-image: url(${(props) => props.url});
 `
 const MusicInfo = styled.div`
-  background-color: rgb(0, 0, 77);
-  width: 450px;
+  //background-color: rgb(0, 0, 77);
+  //background-image: linear-gradient(45deg, white , transparent, transparent, transparent, white );
+  width: 500px;
   height: 200px;
+  display: flex;
+  flex-flow: row wrap;
+`
+const InfoInput = styled.input`
+  padding: 5px;
+  margin: 5px;
+  border-image: linear-gradient(10deg, white 1%, transparent 25%, transparent 75%,white 99%) 10%10%;
+  border-width: 2px;
+  background-color: transparent;
+  //background-image:radial-gradient(circle,rgba(0,0,81,20),rgba(0,0,81,0)) ;
+  color:white;
+  font-size: large;
+  height: 1lh;
+  text-align: center;
+  &:hover{
+    border-image: linear-gradient(10deg, white 1%, transparent 35%, transparent 65%,white 99%) 10% 10%;
+  }
+  &:focus{
+    border-image: linear-gradient(10deg, white 5%, transparent 45%, transparent 55%,white 95%) 10% 10%;
+    outline: none;
+  }
+  &:first-child{
+    flex-basis: 100%;
+  }
+  transition: border-image 1s;
+  &::placeholder{
+    color:rgba(255,255,255,25%);
+    //text-align: center;
+  }
+`
+const InfoInputGradient = styled.input`
+  border-image: linear-gradient(45deg, white , transparent, white );
+  background-image: linear-gradient(45deg, white , transparent, white );
+`
+const InfoDivGradient = styled.div`
+  border-image: radial-gradient(rgba(0,0,0,0) 0%, rgba(0,0,0,0) 34.48%, rgba(0,0,0,0) 100%);
+  background-image: linear-gradient(top to bottom,red , transparent, red);
 `
 
+const trackReducer = (state,action) =>{
+    switch (action.type){
+        case("setAttrib"):
+            console.log("State: ",state);
+            return {
+                ...state,
+                [action.attrib]: action.value
+            }
+        case("setTags"):
+            return {
+                ...state,
+                title: action.tags.title,
+                artist: action.tags.artist,
+                album: action.tags.album,
+            }
+    }
+}
+
+
 function MusicDisplay(props){
-    const [imageSource,setImageSource] = useState("")
-    const [audioSource,setAudioSource] = useState("")
+    const [track, trackDispatch] = useReducer(trackReducer,{},undefined)
 
     const dropImage=async (e)=>{
         e.preventDefault()
@@ -51,12 +107,10 @@ function MusicDisplay(props){
     const readImage=(file)=>{
         const reader = new FileReader();
         reader.onloadend=()=>{
-            // console.log(reader.result)
             let img = document.createElement("img");
             img.onload = function () {
                 let canvas = document.createElement("canvas");
                 let ctx = canvas.getContext("2d");
-                // console.log(img.width)
                 canvas.height=360;
                 canvas.width=360;
 
@@ -64,7 +118,7 @@ function MusicDisplay(props){
 
 
                 let uri = canvas.toDataURL();
-                setImageSource(uri)
+                trackDispatch({type:"setAttrib",attrib:"image",value:uri})
             }
             img.onerror=(error)=>{
                 console.error(error)
@@ -74,16 +128,11 @@ function MusicDisplay(props){
         }
         reader.readAsDataURL(file);
     }
-    const audio = document.createElement("audio");
     const readAudio=(file)=>{
-        // const url = URL.createObjectURL(file)
-        // setAudioSource(url)
-        console.log(file)
         const reader = new FileReader();
         reader.onloadend=()=>{
-
-            // let audioBlob = new Blob(reader.result, {type: 'audio/wav'})
-            setAudioSource(reader.result)
+            // console.log(reader.result)
+            trackDispatch({type:"setAttrib",attrib:"audio",value:reader.result})
         }
         reader.readAsDataURL(file);
 
@@ -97,26 +146,42 @@ function MusicDisplay(props){
         return o;
     }
     const getMetaData=(file)=>{
-        var blob = file.slice(file.size - 128, file.size);
         var reader = new FileReader();
-        reader.onloadend = function(evt) {
-            var buff = evt.target.result;
-            let mp3Tag = new MP3Tag(buff, true)
+        reader.onloadend = async function () {
+            let buff = reader.result;
+            let mp3Tag = new MP3Tag(buff)
+            mp3Tag.read();
+            trackDispatch({type: "setTags", tags: mp3Tag.tags})
+            if (mp3Tag.error !== '') throw new Error(mp3Tag.error)
         }
-        // reader.onloadend=()=>{
-        //     let length = reader.result.byteLength;
-        //     let dv = new DataView(reader.result,length-128, 128)
-        // }
-        console.log(blob)
-        reader.readAsArrayBuffer(blob)
+        reader.readAsArrayBuffer(file)
+    }
+    const writeMetaData= async (buffer)=>{
+        let mp3Tag = new MP3Tag(buffer,true)
+        mp3Tag.read()
+        mp3Tag.tags.album= "BlackBolshevik"
+        mp3Tag.save()
+        mp3Tag.read()
+        let blob = new Blob([mp3Tag.buffer])
+        let a = document.createElement("a");
+        a.download="Track2.wav"
+        a.href= window.URL.createObjectURL(blob)
+        document.body.appendChild(a)
+        a.click()
+        a.remove()
+        trackDispatch({type: "setTags", tags: mp3Tag.tags})
     }
     return(
         <DisplayBody>
-            <MusicBox url={imageSource} onDrop={dropImage} onDragOver={e=>e.preventDefault()} >
+            <MusicBox url={track.image} onDrop={dropImage} onDragOver={e=>e.preventDefault()} >
 
             </MusicBox>
-            <audio id={"audio"} src={audioSource} controls/>
-            <MusicInfo></MusicInfo>
+            <audio id={"audio"} src={track.audio} controls/>
+            <MusicInfo>
+                <InfoInput placeholder={"Track Title"} value={track.title}/>
+                <InfoInput placeholder={"Artist"} value={track.artist}/>
+                <InfoInput placeholder={"Album"} value={track.album}/>
+            </MusicInfo>
         </DisplayBody>
     )
 }export default MusicDisplay;
